6a3ce1ba8d8ef1e7e783446db7b8a7844d951839
yuli <julie.nguyen@coding-academy.fr>
2019-09-18 16:04:11 +0200

    Modify v1 register page

--- src/component/register.js
+++ src/component/register.js
@@ -1,1 +1,31 @@
-import React, { Component } from "react"
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+function Register() {
+    return (
+        <div className="formCenter">
+            <form className="formFields" onSubmit={handleSubmit}>
+                <div className="formField">
+                    <label className="formField_label" htmlFor="pseudo"> Pseudonyme </label>
+                    <input type="text" minLength="3" maxLength="32" id="username" className="formField_input" placeholder="Entrer votre pseudonyme" name="username" value={values.username} onChange={handleChange} />
+                </div>
+                <div className="formField">
+                    <label className="formField_label" htmlFor="email"> Email </label>
+                    <input type="email" id="email" className="formField_input" placeholder="Entrer votre email" name="email" value={values.email} onChange={handleChange} />
+                </div>
+                <div className="formField">
+                    <label className="formField_label" htmlFor="password"> Password </label>
+                    <input type="password" minLength="8" maxLength="32" id="password" className="formField_input" placeholder="Entrer votre password" name="password" value={values.password} onChange={handleChange} />
+                </div>
+                <div className="formField">
+                    <button className="formField_button"> Inscription </button> <Link to="/login" className="formField_link mr-20">Déja inscrit ?</Link>
+                </div>
+            </form>
+        </div>
+
+    );
+}
+
+export default Register;
