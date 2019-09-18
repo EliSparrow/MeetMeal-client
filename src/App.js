f875eaa95faba7c2eb139ffe00907a779e486cb8
Laura <laura.boudier@coding-academy.fr>
2019-09-18 19:49:32 +0200

    added Footer in App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Login from './component/login/login.js';
+import Footer from './component/layout/footer.js'
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <Footer />
     </div>
   );
 }
