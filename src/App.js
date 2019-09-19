fedc1f0091f5631cfd78924206ef29422e81a47f
yuli <julie.nguyen@coding-academy.fr>
2019-09-19 17:45:07 +0200

    Added balise Header to HP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import image from './test.png';
 
 import Login from './component/login/login.js';
+import Header from './component/layout/header.js'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      {/* <img src={image} alt="test"/> */}
       <Login />
     </div>
   );
