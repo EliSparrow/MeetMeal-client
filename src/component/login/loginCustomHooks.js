83cd9813b099eec72fd37ddaeba0b795b00b1234
Laura <laura.boudier@coding-academy.fr>
2019-09-20 20:20:46 +0200

    LOGIN DONE : functionnal and connected to backend , checks done

--- src/component/login/loginCustomHooks.js
+++ src/component/login/loginCustomHooks.js
@@ -1,15 +1,29 @@
 import {useState} from 'react';
+import axios from 'axios';
 
 const useLoginForm = (callback) => {
   const [inputs, setInputs] = useState({});
 
   const handleSubmit = (event) => {
-    if (event) {
+    if (event)
       event.preventDefault();
-    }
+
     checkInputs();
-    console.log(inputs.email);
-    console.log(inputs.password);
+    axios.post('http://localhost:1509/users/login', {
+      email: inputs.email,
+      password: inputs.password
+      },
+    ).then(res => {
+      sessionStorage.setItem('token', res.data.token);
+    }).catch(err => {
+      if (err.response.data.msg) {
+        if (err.response.data.msg === 'Mot de passe invalide')
+          return alert('Mot de passe invalide')
+        else
+          return alert('Utilisateur inconnu')
+      }
+      return alert('Nous sommes désolés, nous faisons face à un problème de serveur')
+    });
   }
 
   const handleInputChange = (event) => {
@@ -19,15 +33,13 @@ const useLoginForm = (callback) => {
 
   const checkInputs = (event) => {
     if (!inputs.email && !inputs.password)
-      alert('Les champs "email" et "mot de passe" sont manquants')
+      return alert('Les champs "email" et "mot de passe" sont manquants');
     else if (!inputs.email)
-      alert('Le champ "email" est manquant')
+      return alert('Le champ "email" est manquant');
     else if (!inputs.password)
-      alert('La champ "mot de passe" est manquant')
+      return alert('La champ "mot de passe" est manquant');
     else
-      return false
-
-    return true
+      return true
   }
 
   return {
