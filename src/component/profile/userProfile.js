98bdea04eb49cd7350698365508c452b6f25a1a1
Laura <laura.boudier@coding-academy.fr>
2019-09-23 14:36:16 +0200

    page user profile

--- src/component/profile/userProfile.js
+++ src/component/profile/userProfile.js
@@ -3,41 +3,53 @@ import {BrowserRouter as Route} from 'react-router-dom';
 import {useState} from 'react';
 import axios from 'axios';
 
-const getUserProfile = (setProfile) => {
+import '../../stylesheets/userProfile.css';
+
+const getUserProfile = (setProfiles => {
   const header = {
     'x-auth-token': sessionStorage.getItem('token')
   }
   axios.get('http://localhost:1509/users/my-profile',
-    { header: header}
+    { headers: header},
+    console.log('je commence axios')
   ).then(res => {
-    setProfile(res.data)
+    console.log('je suis dans le then');
+    setProfiles(res.data)
+    console.log(res.data);
   }).catch(err => {
     alert('Nous sommes désolés, nous faisons face à un problème de serveur')
     return (
       <Route exact path='/'></Route>
     )
   })
-};
+});
 
 const UserProfile = () => {
   let [profiles, setProfiles] = useState([])
-  if (profiles.length === 0)
-    getUserProfile(setProfiles)
+  console.log('let profile : ');
+  console.log(profiles);
+    if (profiles.length === 0){
+      getUserProfile(setProfiles)
+      console.log('let profile après if : ');
+      console.log(profiles);
+    }
 
   return (
-    <div className='user-profile' id='user-profile'>
-      {
-        profiles.map((profile) =>
-          <div className='row'>
-            <div className='info-profile' id='info-profile'>
-              <div className='user-name'><p> First name : </p> {profile.firstname}</div>
-              <div className='user-name'><p> Last name : </p> {profile.lastname}</div>
-            </div>
+    <div className='container user-profile'>
+      <div className='row'>
+          <div className='col-lg-1 user-info-intro'>
+            <img className='image' src={profiles.avatar}></img>
           </div>
-        )
-      }
+          <div className='col-lg-8 user-info-intro'>
+            <h1 className='user-name'> {profiles.firstname} {profiles.lastname}</h1>
+          </div>
+          <div className='col-lg-3 info-profile' id='info-profile'>
+            <div className='user-name'><p> Situation amoureuse :  {profiles.loveStatus}</p></div>
+          </div>
+
+      </div>
     </div>
-  )
+  );
 }
 
 export default UserProfile;
