e188a64c25081da4780221fc5401e5aa17f844cd
PetiteYuli <yunguyen94@gmail.com>
2019-09-23 19:33:51 +0200

    fixed error className in Header

--- src/component/layout/header.js
+++ src/component/layout/header.js
@@ -33,7 +33,7 @@ const Header = () => {
           <Dropdown.Item eventKey="7" href="/">Créer un repas</Dropdown.Item>
           <Dropdown.Item eventKey="8" href="/">Voir tous les membres</Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Item eventKey="4"><i class="fa fa-sign-in"></i> Se déconnecter</Dropdown.Item>
+          <Dropdown.Item eventKey="4"><i className="fa fa-sign-in"></i> Se déconnecter</Dropdown.Item>
         </DropdownButton>
       </Nav>
     </Navbar.Collapse>
