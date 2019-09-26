9be0d82d76304ca0419586db436b5ef5ce24402e
laurabdier <laura.boudier@coding-academy.fr>
2019-09-26 19:17:46 +0200

    updated App Component : everything in a ROUTE div

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ import ShowEvent from './component/event/showEvent.js';
 function App() {
   return (
     <Router>
-      <div className="App">
       <Route component={Header}></Route>
+      <div id="content">
         <PublicRoute restricted={false} component={Home} path="/" exact />
         <PublicRoute restricted={true} component={Login} path="/login" exact />
         <PublicRoute restricted={true} exact path="/register" component={Register}/>
@@ -30,10 +30,9 @@ function App() {
         <PrivateRoute exact path='/create-event' component={CreateEvent}></PrivateRoute>
         <PrivateRoute exact path='/list-events' component={ListEvents}></PrivateRoute>
         <PrivateRoute exact path='/event/:eventId' component={ShowEvent}></PrivateRoute>
-
+      </div>
 
       <Route component={Footer}></Route>
-      </div>
     </Router>
   );
 }
