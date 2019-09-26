3dc15a71690ba6e2c1d28975d1665852e11eb467
laurabdier <laura.boudier@coding-academy.fr>
2019-09-26 20:13:15 +0200

    created new component HomeSearchBar to try to integrate Emily's CSS

---
+++ src/component/home/searchBarHome.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+import '../../stylesheets/searchBarHome.css'
+
+class SearchBarHome extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  render(){
+    return (
+      <div class="s002">
+        <form class="form-inline">
+          <div class="inner-form">
+
+          <h1>SEARCH BAR </h1>
+
+          </div>
+        </form>
+       </div>
+    )
+  }
+
+}
+
+export default SearchBarHome;
