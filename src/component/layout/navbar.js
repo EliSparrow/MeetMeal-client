95b2c70cd7bf15a2740bdb8ddaead32c2020a868
EliSparow <max.pp@hotmail.fr>
2019-09-30 20:07:39 +0200

    cleaning css for navbar

--- src/component/layout/navbar.js
+++ src/component/layout/navbar.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import image from '../../images/MeatMeal.png'
-import '../../stylesheets/navbar.css';
+// import '../../stylesheets/navbar.css';
+import '../../stylesheets/navbar.scss';
 
 class Navbar extends Component {
   constructor(props){
@@ -35,41 +36,86 @@ class Navbar extends Component {
   }
 
   render(){
+    // const userLinks = (
+    //   <div>
+    //           <a className="nav-link dropdown-toggle" href="/profile" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    //             {/* {this.state.name} */}
+    //             <i className="fa fa-user fa-fw"></i>
+    //           </a>
+    //           <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+    //             <a className="dropdown-item" href="/edituser">Editer son profile</a>
+    //             <a className="dropdown-item" href="/profile">voir mon profil</a>
+    //             <a className="dropdown-item" href="/">Mon historique</a>
+    //             <a className="dropdown-item" href="/login" onClick={this.logout}>Se déconnecter</a>
+    //           </div>
+    //   </div>
+    // )
+
     const userDropdown = (
-      <div className='border center'>
-          <ul className='border'>
-            <i className="fa fa-user fa-fw"></i>
-              <a className="nav-link dropdown-toggle" href="/profile" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {this.state.name}
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="/edituser">Editer son profile</a>
-                <a className="dropdown-item" href="/listusers">Voir les profiles</a>
-                <a className="dropdown-item" href="/profile">voir mon profil</a>
-                <a className="dropdown-item" href="/">Mon historique</a>
-                <a className="dropdown-item" href="/login" onClick={this.logout}>Se déconnecter</a>
-              </div>
-          </ul>
+      <div className='my-2 my-lg-0'>
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-user fa-lg"></i></a>
+            <div className="dropdown-menu dropdown-menu-right">
+              <a className="dropdown-item" href="/profile">Voir mon profil</a>
+              <a className="dropdown-item" href="/edituser">Editer son profile</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="/login" onClick={this.logout}>Se déconnecter</a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/login" onClick={this.logout}><i className="fa fa-sign-out fa-lg"></i></a>
+          </li>
+        </ul>
       </div>
     )
 
-    const regularNav = (
-      <div className='border center'>
-          <a className="nav-link" href="/register">Inscritpion</a>
-          <a className="nav-link" href="/login">Connexion</a>
+    const userLinks = (
+      <>
+        <li className="nav-item">
           <a className="nav-link" href="/">Rechercher un repas</a>
-      </div>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/listusers">Voir les profiles</a>
+        </li>
+      </>
+    )
+
+    const offlineLinks = (
+        <>
+          <li className="nav-item">
+            <a className="nav-link" href="/login">Connexion</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/register">Inscritpion</a>
+          </li>
+        </>
     )
 
     return(
-        <div>
-          <nav>
-            <a className='image-navbar' href='/'>
-              <img src={image} alt='logo' href='/'></img>
-            </a>
-            {localStorage.token ? userDropdown : regularNav}
-          </nav>
-        </div>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <a className='navbar-brand' href='/'>
+            <img className='navLogo' src={image} alt='logo' href='/'></img>
+          </a>
+        
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="/">Acceuil <span className="sr-only">(current)</span></a>
+              </li>
+
+              { localStorage ? userLinks : offlineLinks }
+            </ul>
+            
+              { localStorage ? userDropdown : null }
+            
+            </div>
+        </nav>
+      </div>
     )
   }
 
