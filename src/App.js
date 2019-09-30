aa5a50ddf77322b8abf20a44c6d5911495e4fa10
laurabdier <laura.boudier@coding-academy.fr>
2019-09-30 18:53:35 +0200

    finally removed route for UserEvents component because i directly included it in UserProfile component render

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import ListEvents from './component/event/listEvent.js';
 import ShowEvent from './component/event/showEvent.js';
 import { EditProfile } from './component/profile/EditProfile.js';
 import { DeleteUser } from './component/profile/DeleteUser.js';
-import UserEvents from './component/profile/my-profile/userEvents.js'
 
 
 function App() {
@@ -40,7 +39,6 @@ function App() {
         <PrivateRoute exact path='/create-event' component={CreateEvent}></PrivateRoute>
         <PrivateRoute exact path='/list-events' component={ListEvents}></PrivateRoute>
         <PrivateRoute exact path='/event/:eventId' component={ShowEvent}></PrivateRoute>
-        <PrivateRoute exact path='/profile' component={UserEvents}></PrivateRoute>
       </div>
       <Route component={Footer}></Route>
     </Router>
