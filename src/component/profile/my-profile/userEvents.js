bf109220ff7ddb4f7a5c92201e30f64bedd260b2
laurabdier <laura.boudier@coding-academy.fr>
2019-09-30 18:27:19 +0200

    created the button so now you can choose if you want to show either events where the user went or the ones he created

--- src/component/profile/my-profile/userEvents.js
+++ src/component/profile/my-profile/userEvents.js
@@ -6,26 +6,30 @@ import EventsJoined from './eventsJoined';
 class UserEvents extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+          status: true
+        };
     }
 
-    showJoinedEvents(event, status) {
-        status = false;
-        console.log(status)
+    showJoinedEvents = (event) => {
+        event.preventDefault();
+        this.setState({status: false})
+        console.log('SHOW JOINED EVENTS SO MUST BE FALSE : ', this.state.status)
     }
 
-    showCreatedEvents(event, status) {
-        status = true;
-        console.log(status)
+    showCreatedEvents = (event, status) => {
+      event.preventDefault();
+      this.setState({status: true})
+      console.log('SHOW JOINED EVENTS SO MUST BE TRUE : ', this.state.status)
     }
 
     render() {
-        var status = true; 
+        console.log('status : ', this.state.status);
         return (
             <div className="container">
                 <button type="button" onClick={this.showCreatedEvents}>Repas créés</button>
                 <button type="button" onClick={this.showJoinedEvents}>Repas rejoints</button>
-                {status == true ? <EventsCreated/> : <EventsJoined/>}
+                {this.state.status == true ? <EventsCreated/> : <EventsJoined/>}
             </div>
         )
     }
