bb9a1ce1d64425a493ae4f9de6dbd49026b263da
fabolododo <fabolododo@hotmail.fr>
2019-10-01 15:28:12 +0200

    added admin

---
+++ src/component/admin/AdminCreateForm.js
@@ -0,0 +1,171 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+
+export class AdminCreateForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: null,
+            firstname: "",
+            lastname: "",
+            age: 0,
+            email: "",
+            avatar: "",
+            bio: "",
+            loveStatus: "",
+            zipCode: 0,
+            address: "",
+            city: "",
+            password: "",
+            toquesAvailable: 0
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            firstname: this.state.firstname,
+            newFirstname: this.state.firstname,
+            lastname: this.state.lastname,
+            newLastname: this.state.lastname,
+            age: this.state.age,
+            newAge: this.state.age,
+            email: this.state.email,
+            newEmail: this.state.email,
+            avatar: this.state.avatar,
+            newAvatar: this.state.avatar,
+            bio: this.state.bio,
+            newBio: this.state.bio,
+            loveStatus: this.state.loveStatus,
+            newLoveStatus: this.state.loveStatus,
+            zipCode: this.state.zipCode,
+            newZipCode: this.state.zipCode,
+            address: this.state.address,
+            newAddress: this.state.address,
+            city: this.state.city,
+            newCity: this.state.city,
+            password: this.state.password,
+            newPassword: this.state.password,
+            toquesAvailable: this.state.tocqueAvailable,
+            newToquesAvailable: this.state.tocqueAvailable
+        });
+
+        const header = {
+            'x-auth-token': localStorage.getItem('token')
+        }
+        axios.get(process.env.REACT_APP_API + '/users/my-profile',
+            { headers: header},
+        ).then(res => {
+              this.setState({ user: res.data });
+          })
+          .catch(err => {
+              console.error(err);
+          })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+    }
+
+
+
+    handleEditUser = e => {
+        e.preventDefault();
+
+        const header = {
+            'x-auth-token': localStorage.getItem('token')
+        }
+
+        var editUser = {
+            firstname: this.state.newFirstname,
+            lastname: this.state.newLastname,
+            age: parseInt(this.state.newAge),
+            email: this.state.newEmail,
+            avatar: this.state.newAvatar,
+            bio: this.state.newBio,
+            loveStatus: this.state.newLoveStatus,
+            address: this.state.newAddress,
+            zipCode: parseInt(this.state.newZipCode),
+            city: this.state.newCity,
+            password: this.state.newPassword,
+            toquesAvailable: parseInt(this.state.newToquesAvailable),
+            id : this.state.user._id
+        };
+        console.log(editUser.address);
+        console.log(editUser.id);
+
+        axios.put(process.env.REACT_APP_API + `/users/`+ editUser.id, editUser, { headers: header })
+            .then(res => {
+                alert('Votre profil a été modifié');
+                this.setState({ user: res.data });
+                this.props.history.push('/profile');
+            })
+            .catch(err => {
+                console.error(err.response);
+            })
+
+    }
+
+    render() {
+        var {
+            user
+        } = this.state
+
+        
+        return(
+            <div className="container">
+                 {user ? (
+                <div className= 'row'>
+                    <form onSubmit={ this.handleEditUser }>
+                        <div className='title'>
+                            <h1>Modifiez votre profile: </h1>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='text' defaultValue={user.firstname} name='newFirstname' placeholder='firstname' className='input-firstname' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='text' defaultValue={user.lastname} name='newLastname' placeholder='lastname' className='input-lastname' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='text' defaultValue={user.age} name='newAge' className='input-age' placeholder='age' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='email' defaultValue={user.email} name='newEmail' className='input-email' placeholder='email' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='text' defaultValue={user.avatar} name='newAvatar' className='input-avatar' placeholder='avatar' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='textarea' defaultValue={user.bio} name='newBio' className='input-bio' placeholder='biographie' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='text' defaultValue={user.loveStatus} name='newLoveStatus' className='input-loveStatus' placeholder='Situation Amoureuse' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='text' defaultValue={user.zipCode} name='newZipCode' className='input-zipCode' placeholder='Code Postal' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='text' defaultValue={user.address} name='newAddress' className='input-address' placeholder='Adresse' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='text' defaultValue={user.city} name='newCity' className='input-city' placeholder='Ville' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='password' name='newPassword' className='input-password' placeholder='Mot de Passe' onChange={ this.handleChange }></input>
+                        </div>
+                        <div className='col lg-4 user-inputs'>
+                            <input type='text' defaultValue={user.toquesAvailable} name='newToquesAvailable' className='input-toquesAvailable' placeholder='Monnaie Toque' onChange={ this.handleChange }></input>
+                        </div>
+                        <button className="submit">Modifiez votre profile</button>
+                        <button className="reset"><Link redirect to='/edituser'>Annuler</Link></button>
+                    </form>
+                </div>
+                 ) : null }
+            </div>
+        )
+    }
+}
---
+++ src/component/admin/AdminLanding.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { ListUsers } from '../profile/ListUsers';
+import { EditProfile } from '../profile/EditProfile';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { AdminCreateForm } from './AdminCreateForm';
+
+export class AdminLanding extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            user: null,
+            admin : false,
+            showListBool: true,
+            showUpdateBool: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({ user: this.state.user });
+        const header = {
+            'x-auth-token': localStorage.getItem('token')
+          }
+
+        axios.get(process.env.REACT_APP_API + '/users/my-profile', { headers: header })
+          .then((res) => {
+              console.log(res.data.admin);
+              this.setState({
+                  user : res.data,
+                admin : res.data.admin })
+          })
+          .catch((err) => {
+              console.error(err);
+          })
+    }
+    delUserList = () => {
+        this.setState({
+            showListBool : false})
+    }
+    
+    render() {
+        let UsersList;
+        if (this.state.showListBool){
+            UsersList = <ListUsers/>
+        }
+        else {
+            UsersList = <AdminCreateForm/>
+        }
+        const { user, admin } = this.state
+
+        return(
+            <div>
+                { admin ? (
+                    <div className='test'>
+                        <h1 className='admintitle'>Salut Admin</h1>
+                        <p>Salut Admin</p>
+                        <div className="Listusers">
+                            { UsersList }
+                        </div>
+                        <div className='testButton'>
+                            <button className='button' onClick={this.delUserList}>Test</button>
+                        </div>
+
+                    </div>
+                ) : (
+                    <div className='testadminFalse'>
+                        <h1>t'as pas le droit</h1>
+                        <h1>t'as pas le droit</h1>
+                        <h1>t'as pas le droit</h1>
+                        <h1>t'as pas le droit</h1>
+                        <h1>t'as pas le droit</h1>
+                        <h1>t'as pas le droit</h1>
+                        <h1>t'as pas le droit</h1>
+                        <h1>t'as pas le droit</h1>
+                        <h1>t'as pas le droit</h1>
+                        <h1>t'as pas le droit</h1>
+                    </div>
+                ) }
+            </div>
+        )
+    }
+}
