c36e608d8aa4f48683339fed2c857bcb9ee0c293
PetiteYuli <yunguyen94@gmail.com>
2019-10-01 12:10:46 +0200

    Added pages contact team present on the footer

---
+++ src/component/layout/team.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Team = () => {
+return (
+    <div className="row" style={{marginTop: 80 + 'px', textAlign: 'center'}} >
+    <div className="card col-6" style={{width: 25 + 'em'}}>
+    <img className="card-img img-fluid"  src="https://media.comicbook.com/2019/04/netflix-rilakkuma-and-kaoru-1167403-1280x0.jpeg" alt="avatar" />
+    <div className="card-body">
+        <h5 className="card-title">Maxime André</h5>
+        <p className="card-text"> Helloooo</p>
+    </div>
+</div>
+<div className="card col-6" style={{width: 25 + 'em'}}>
+    <img className="card-img img-fluid"  src="https://media.comicbook.com/2019/04/netflix-rilakkuma-and-kaoru-1167403-1280x0.jpeg" alt="avatar" />
+    <div className="card-body">
+        <h5 className="card-title">Laura BOUDIER</h5>
+        <p className="card-text"> AAAAAH !</p>
+    </div>
+</div>
+<div className="card col-4" style={{width: 25 + 'em'}}>
+    <img className="card-img img-fluid"  src="https://media.comicbook.com/2019/04/netflix-rilakkuma-and-kaoru-1167403-1280x0.jpeg" alt="avatar" />
+    <div className="card-body">
+        <h5 className="card-title">Max DE PONFILLY</h5>
+        <p className="card-text">Bonjour ! </p>
+    </div>
+</div>
+<div className="card col-4" style={{width: 25 + 'em'}}>
+    <img className="card-img img-fluid"  src="https://media.comicbook.com/2019/04/netflix-rilakkuma-and-kaoru-1167403-1280x0.jpeg" alt="avatar" />
+    <div className="card-body">
+        <h5 className="card-title">Emilie HAYOUN</h5>
+        <p className="card-text">HELLOOOOOO</p>
+    </div>
+</div>
+<div className="card col-4" style={{width: 25 + 'em'}}>
+    <img className="card-img img-fluid"  src="https://media.comicbook.com/2019/04/netflix-rilakkuma-and-kaoru-1167403-1280x0.jpeg" alt="avatar" />
+    <div className="card-body">
+        <h5 className="card-title">Fabrice LATRI</h5>
+        <p className="card-text">Allo ?</p>
+    </div>
+</div>
+<div className="card col-6" style={{width: 25 + 'em'}}>
+    <img className="card-img img-fluid"  src="https://media.comicbook.com/2019/04/netflix-rilakkuma-and-kaoru-1167403-1280x0.jpeg" alt="avatar" />
+    <div className="card-body">
+        <h5 className="card-title">Julie NGUYEN</h5>
+        <p className="card-text">Cat Lover !</p>
+    </div>
+</div>
+<div className="card col-6" style={{width: 25 + 'em'}}>
+    <img className="card-img img-fluid"  src="https://media.comicbook.com/2019/04/netflix-rilakkuma-and-kaoru-1167403-1280x0.jpeg" alt="avatar" />
+    <div className="card-body">
+        <h5 className="card-title">Lisa TOULZAC</h5>
+        <p className="card-text">Homo du groupe !</p>
+    </div>
+</div>
+
+</div>
+)
+}
+
+export default Team
