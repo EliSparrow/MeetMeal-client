c90a21abd4294974b6dd14876e1e783e171fb7fb
laurabdier <laura.boudier@coding-academy.fr>
2019-10-03 11:12:34 +0200

    functionality susbcribe to an event DONE + button "join the event" or "see the event" depens on if you created the event, are already subscribed or if you stil can subscribe

--- src/component/event/cardEvent.js
+++ src/component/event/cardEvent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import '../../stylesheets/cardEvent.css';
 
@@ -20,11 +20,29 @@ class CardEvent extends Component {
       numberMaxOfGuests: "",
       typeOfMeal: "",
       typeOfCuisine: "",
-      user: null
+      guests: "",
+      comments: "",
+      user: null,
+      joinedMeals: false,
+      createdMeals: false
     }
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+    const header = {
+      'x-auth-token': localStorage.getItem('token')
+    }
+
+    await axios.get('http://localhost:1509/users/my-profile', { headers: header })
+          .then((res) => {
+            this.setState({ user: res.data })
+            // console.log('response du get user : ', res.data);
+            // console.log('user id : ', this.state.user._id);
+          })
+          .catch((err) => {
+            console.error(err.response);
+          })
+
     this.setState({
       _id: this.props._id,
       firstname: this.props.firstname,
@@ -36,46 +54,102 @@ class CardEvent extends Component {
       cost: this.props.cost,
       numberMaxOfGuests: this.props.numberMaxOfGuests,
       typeOfMeal: this.props.typeOfMeal,
-      typeOfCuisine: this.props.typeOfCuisine
+      typeOfCuisine: this.props.typeOfCuisine,
+      guests: this.props.guests,
+      comments: this.props.comments
     })
-    console.log(this.props._id);
-    this.getUser();
-  }
 
+
+  // get all events where the user already subscribed
+  // await axios.get(process.env.REACT_APP_API + '/events/' + this.state.user._id + '/guestsEvents')
+  //       .then( res => {
+  //         this.setState({joinedMeals: res.data})
+  //         console.log('les events que le user a deja rejoint : ', this.state.joinedMeals);
+  //       }).catch( err => {
+  //         console.log(err.response);
+  //       })
+  //
+  // get all events created by the user connected
+  // await axios.get(process.env.REACT_APP_API + '/events/' + this.state.user._id + '/showEvents')
+  //       .then( res => {
+  //         this.setState({createdMeals: res.data})
+  //         console.log('les events que le user a deja créé : ', this.state.createdMeals);
+  //       }).catch( err => {
+  //         console.log(err.response);
+  //       })
+    const url = process.env.REACT_APP_API + '/events/' + this.state._id + '/addGuest';
+     axios.put(url, null,
+                { headers: header }
+                ).then( res => {
+                  console.log(res.data);
+                }).catch( err => {
+                  if (err.response.data.msg === 'Vous ne pouvez pas rejoindre votre propre evenement')
+                    this.setState({createdMeals: true})
+                  else if (err.response.data.msg === 'Vous etes deja inscrit a cet evenement')
+                    this.setState({joinedMeals: true})
+                  else
+                    console.log(err.response);
+                })
+
+}
+
+  // Go on the presentation page of an event
   handleSubmit = async (event) => {
    event.preventDefault();
-    console.log(this.state._id);
+    // console.log(this.state._id);
     const url = '/event/:' + this.state._id.toString() ;
     console.log(url);
     this.props.history.push(url)
   }
 
-  getUser = async () => {
+  // Edit an event
+  checkEdit = () => {
+    const { user, _id } = this.state;
+    if ( user && (user._id === this.props.user._id)){
+      return(
+        <div className='edit-button'>
+          <a href={"/editevent/" + _id}><button className="btn btn-warning">Modifiez votre évènement!</button></a>
+        </div>
+      )
+    }
+    console.log(this.props.user._id)
+  }
+
+
+// Function to subscribe to an event :
+  subscribeToAnEvent = async (event) => {
+    event.preventDefault();
     const header = {
       'x-auth-token': localStorage.getItem('token')
     }
-    console.log(this.props.user._id);
-    Axios.get(process.env.REACT_APP_API + '/users/my-profile', { headers: header })
-      .then((res) => {
-        this.setState({ user: res.data })
-        console.log(res.data);
-        console.log(this.props.user._id)
-      })
-      .catch((err) => {
-        console.error(err);
-      })
-  }
 
-  checkEdit = () => {
-    const { user } = this.state;
-    if (( user && (user._id === this.props.user._id)) || (user && (user.admin === true))){
-      return(
-        <div className='edit-button'>
-          <Link to ={"/editevent/" + this.props._id }>
-            <button type="submit">Je modifie mon évènement !</button>
-          </Link>
-        </div>
-      )
+    const userInfo = await axios.get(process.env.REACT_APP_API + '/users/my-profile',
+          { headers: header})
+
+    const userId = userInfo.data._id;
+    const userToquesAvailable = userInfo.data.toquesAvailable
+          console.log('id + toques : ', userId, userToquesAvailable);
+
+    console.log('prix : ', this.state.cost);
+
+    if (userToquesAvailable < this.state.cost)
+      alert("Vous n'avez pas assez de toques disponibles pour vous inscire à cet évenement.")
+    else{
+    const url = process.env.REACT_APP_API + '/events/' + this.state._id + '/addGuest';
+      await axios.put(url, null,
+                  { headers: header }
+                  ).then( res => {
+                    console.log(res.data);
+                  }).catch( err => {
+                    if (err.response.data.msg === 'Vous ne pouvez pas rejoindre votre propre evenement')
+                      alert('Vous ne pouvez pas rejoindre votre propre evenement');
+                    else if (err.response.data.msg === 'Vous etes deja inscrit a cet evenement')
+                      alert('Vous etes deja inscrit a cet evenement')
+                    else if ( err.response.data.msg === "Nombre maximum d'invites deja atteint")
+                      alert("Nombre maximum d'invites deja atteint")
+                    else
+                      console.log(err.response);
+                  })
     }
   }
 
@@ -94,30 +168,36 @@ class CardEvent extends Component {
       typeOfCuisine
     } = this.state;
 
-    return (
+    var pickButton = () => {
+      console.log('pick button worked');
+      if( this.state.joinedMeals === true )
+          return ( <button type="submit" onClick={this.handleSubmit}>Déjà inscrit ! Voir l'évenement</button>)
+        else if ( this.state.createdMeals === true )
+          return ( <button type="submit" onClick={this.handleSubmit}>Voir votre évenement</button>)
+        else
+          return (<button type="submit" onClick={this.subscribeToAnEvent}>Je rejoins l'évenement !</button>)
+    }
 
-      <div className="card row col-lg-12">
-      <div className="card-body row">
-      <div className='image col-3'>
-        <img src={avatar} alt='user profile avatar'></img>
-        <h6 className="card-subtitle text-muted">{firstname} {lastname}</h6>
-      </div>
-      <div className="col-5">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">Type de repas : {typeOfMeal}</p>
-        <p className="card-text">Type de cuisine : {typeOfCuisine}</p>
-        <p className="card-text">{description}</p>
-        <p className="card-text"><i className="fa fa-map-marker"/> {city}</p>
+    return (
+      <div className="card row" style={{width: 70 + 'em', marginTop: 2 + 'em'}}>
+        <div className="card-body row">
+          <div className='image col-3'>
+            <img src={avatar} alt='user profile avatar'></img>
+            <h6 className="card-subtitle text-muted">{firstname} {lastname}</h6>
+          </div>
+          <div className="col-5">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">Type de repas : {typeOfMeal}</p>
+            <p className="card-text">Type de cuisine : {typeOfCuisine}</p>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><i className="fa fa-map-marker"/> {city}</p>
+          </div>
+          <div className="col-4">
+            <p className="card-text">Pour {numberMaxOfGuests} personnes</p>
+            <p className="card-text">Coût : {cost} Toques</p>
+            {this.state.createdMeals == true ? pickButton() : (<button type="submit" onClick={this.subscribeToAnEvent}>Je rejoins l'évenement !</button>)}
+          </div>
         </div>
-        <div className="col-4">
-        <p className="card-text">Pour {numberMaxOfGuests} personnes</p>
-        <p className="card-text">Coût : {cost} Toques</p>
-        <button type="submit">Je rejoins l'évenement !</button>
-        <div className="checkedit">
-          {this.checkEdit()}
-        </div>
-        </div>
-      </div>
     </div>
     )
   }
