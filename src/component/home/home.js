20bc3c81b21bde92bc86cb58673a41404c7fb77e
Emi Bellissima <emibellissima@gmail.com>
2019-10-03 17:29:49 +0200

    Update import SelectEvent

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SelectEvent from './SelectEvent';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../stylesheets/home.css';
 
